12afd60a29d572b18ce946cda4742ad45fe810a7
canjalal <vincent.shuali@gmail.com>
2022-07-13 10:58:38 -0700

    Seeded database

--- import_db.sql
+++ import_db.sql
@@ -1,17 +1,20 @@
 
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS Question_likes;
+DROP TABLE IF EXISTS Replies;
+DROP TABLE IF EXISTS question_follows; 
+DROP TABLE IF EXISTS Questions ;
+DROP TABLE IF EXISTS Users;
 
-
--- DROP TABLE IF EXISTS Users;
 CREATE TABLE Users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 
-;)
+);
 
--- DROP TABLE IF EXISTS Questions ;
+
 CREATE TABLE Questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
@@ -34,10 +37,12 @@ FOREIGN KEY(question_id) REFERENCES Questions(id)
 
 CREATE TABLE Replies (
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     body TEXT NOT NULL,
     question_id INTEGER NOT NULL,
     parent_id INTEGER,
 
+    FOREIGN KEY (user_id) REFERENCES Users(id)
     FOREIGN KEY (parent_id) REFERENCES Replies(id)
 
 );
@@ -75,8 +80,17 @@ VALUES
      ((SELECT id FROM Users WHERE fname = 'Karen'),(SELECT id FROM Questions WHERE title = 'Discord')),
     ((SELECT id FROM Users WHERE fname = 'Karen'),(SELECT id FROM Questions WHERE title = 'Games'));
 
+INSERT INTO
+Replies (body, user_id, question_id, parent_id)
 
+VALUES
+    ('Next week', (SELECT id FROM Users WHERE fname = 'Vince'), (SELECT id FROM Questions WHERE title = 'Schedule'), NULL),
+    ('They said Monday', (SELECT id FROM Users WHERE fname = 'Jay'), (SELECT id FROM Questions WHERE title = 'Schedule'), (SELECT id FROM Replies WHERE body = 'Next week')),
+    ('Hell yeah', (SELECT id FROM Users WHERE fname = 'Anna'), (SELECT id FROM Questions WHERE title = 'Games'), NULL);
 
-
-
-
+INSERT INTO
+Question_likes (question_id, user_id)
+VALUES
+    ((SELECT id FROM Questions WHERE title = 'Games'), (SELECT id FROM Users WHERE fname = 'Anna')),
+    ((SELECT id FROM Questions WHERE title = 'Games'), (SELECT id FROM Users WHERE fname = 'Vince')),
+    ((SELECT id FROM Questions WHERE title = 'Discord'), (SELECT id FROM Users WHERE fname = 'Karen'));
